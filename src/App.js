c9a78c3056d8c33f92fcacdcd3738587be6a25bc
Sravya Vemuri <sravyasolutions@gmail.com>
2024-02-18 17:47:44 -0500

    Added Axios and Charts.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import Menu from './Menu/Menu';
+import Hero from './Hero/Hero';
+import HomePage from './HomePage/Homepage';
+import Footer from './Footer/footer';
+import AboutPage from './AboutPage/AboutPage';
+import LoginPage from './LoginPage/Loginpage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Menu/>
+      <Hero/>
+      <div className="mainContainer">
+      <Routes>
+      <Route path='/' element={<HomePage/>}></Route>
+          <Route path='/about' element={<AboutPage/>}></Route>
+          <Route path='/login' element={<LoginPage/>}></Route>
+          </Routes>     
+
+      
+
+      </div>
+      
+      <Footer/>
+    </Router>
   );
 }
 
