b01ce6d94e31cd93878a97352b144fd61713489c
Sravya Vemuri <sravyasolutions@gmail.com>
2024-02-18 17:49:25 -0500

    Added Axios and Charts.

---
+++ src/Footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer className="footer" role="contentinfo">
+        <div className="center">
+            All rights reserved &copy; Sravya Vemuri
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
